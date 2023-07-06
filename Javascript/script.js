070e6f02b0705ed5b7a6d44c5671ec7800945480
Rosa Rosales <rosamilib@gmail.com>
2023-07-06 19:44:24 -0400

    Changes in script

--- Javascript/script.js
+++ Javascript/script.js
@@ -26,8 +26,8 @@
 
 'use strict'
 
-const grande = document.querySelector('.grande')
-const punto = document.querySelectorAll('.punto')
+// const grande = document.querySelector('.grande')
+// const punto = document.querySelectorAll('.punto')
 
 // Cuando CLICK en punto
     // Saber la posición de ese punto
@@ -36,25 +36,45 @@ const punto = document.querySelectorAll('.punto')
     // AÑADIR la clase activo al punto que hemos hecho CLICK
 
 // Recorrer TODOS los punto
-punto.forEach( ( cadaPunto , i )=> {
-    // Asignamos un CLICK a cadaPunto
-    punto[i].addEventListener('click',()=>{
+// punto.forEach( ( cadaPunto , i )=> {
+//     // Asignamos un CLICK a cadaPunto
+//     punto[i].addEventListener('click',()=>{
 
-        // Guardar la posición de ese PUNTO
-        let posicion  = i
-        // Calculando el espacio que debe DESPLAZARSE el GRANDE
+//         // Guardar la posición de ese PUNTO
+//         let posicion  = i
+//         // Calculando el espacio que debe DESPLAZARSE el GRANDE
+//         let operacion = posicion * -50
+
+//         // MOVEMOS el grand
+//         grande.style.transform = `translateX(${ operacion }%)`
+
+//         // Recorremos TODOS los punto
+//         punto.forEach( ( cadaPunto , i )=>{
+//             // Quitamos la clase ACTIVO a TODOS los punto
+//             punto[i].classList.remove('activo')
+//         })
+//         // Añadir la clase activo en el punto que hemos hecho CLICK
+//         punto[i].classList.add('activo')
+
+//     })
+// })
+
+const grande = document.querySelector('.grande')
+const punto = document.querySelector('.punto')
+
+// Asignar un clic a todos los puntos, click en punto
+// Saber la posición de ese punto
+// Aplicar un transform translateX al grande
+// Quitar la clase activo de los puntos
+// Añadir clase activo al punto en el que se hace click
+
+punto.forEach( (cadaPunto , i) => {
+    punto[i].addEventListener ('click', ()=> {
+        let posicion = i
+        // Cuando la posición es 0 transformX es 0
+        // Cuando la posición es 1 transformX es -25%
+        // operación = posición * -25
         let operacion = posicion * -50
-
-        // MOVEMOS el grand
-        grande.style.transform = `translateX(${ operacion }%)`
-
-        // Recorremos TODOS los punto
-        punto.forEach( ( cadaPunto , i )=>{
-            // Quitamos la clase ACTIVO a TODOS los punto
-            punto[i].classList.remove('activo')
-        })
-        // Añadir la clase activo en el punto que hemos hecho CLICK
-        punto[i].classList.add('activo')
-
-    })
-})
+        grande.style.transform = `translateX($ {operacion} %)`
+    }) 
+}) 
